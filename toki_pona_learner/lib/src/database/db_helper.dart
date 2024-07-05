5f6fe17fae54b1b0feceb0e599759a996b5cc3d3
tankk <tankk@student.unimelb.edu.au>
2024-07-05 21:05:34 +1000

    include csv processing

--- toki_pona_learner/lib/src/database/db_helper.dart
+++ toki_pona_learner/lib/src/database/db_helper.dart
@@ -1,9 +1,17 @@
 // import 'package:sqflite/sqflite.dart';
+import 'package:flutter/services.dart';
+
 import './database_sql.dart';
 import 'dart:io';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+import 'package:csv/csv.dart';
+
+//import models
+import '../models/word.dart';
+import '../models/part_of_speech.dart';
+import '../models/definition.dart';
 
 class DatabaseHelper {
   static final DatabaseHelper _databaseHelper =
@@ -32,7 +40,7 @@ class DatabaseHelper {
     print("db_helper.dart| db path:$path");
 
     // open/create the database at the given path
-    var db = await openDatabase(path, version: 1, onCreate: _createDb);
+    Database db = await openDatabase(path, version: 1, onCreate: _createDb);
 
     return db;
   }
@@ -42,5 +50,89 @@ class DatabaseHelper {
     await db.execute(createWordsTable);
     await db.execute(createPartsOfSpeechTable);
     await db.execute(createDefinitionsTable);
+    await loadCSVtoDB(db);
+  }
+
+  Future<void> loadCSVtoDB(Database db) async {
+    await insertDefinitions(db);
+    return;
+  }
+
+  Future<void> insertDefinitions(Database db) async {
+    final String defsData =
+        await rootBundle.loadString('assets/toki_pona_dict.csv');
+    List<List<dynamic>> data = const CsvToListConverter().convert(defsData);
+
+    for (var row in data) {
+      String wordStr = row[0];
+      String posStr = row[1];
+      var defsStr = row[2];
+
+      int wordID = await getOrInsertWordId(db, wordStr);
+      int posID = await getOrInsertPartOfSpeechId(db, posStr);
+
+      await db.insert('Definitions', {
+        'word_id': wordID,
+        'part_of_speech_id': posID,
+        'definition': defsStr
+      });
+    }
+  }
+
+  Future<int> getOrInsertWordId(Database db, String word) async {
+    List<Map<String, dynamic>> maps = await db.query(
+      'Words',
+      where: 'word = ?',
+      whereArgs: [word],
+    );
+    if (maps.isNotEmpty) {
+      return maps.first['id'] as int;
+    } else {
+      // insert
+      int newId = await db.insert('Words', {'word': word});
+      return newId;
+    }
+  }
+
+  Future<int> getOrInsertPartOfSpeechId(
+      Database db, String partOfSpeech) async {
+    List<Map<String, dynamic>> maps = await db.query(
+      'PartsOfSpeech',
+      where: 'part = ?',
+      whereArgs: [partOfSpeech],
+    );
+    if (maps.isNotEmpty) {
+      return maps.first['id'] as int;
+    } else {
+      int newId = await db.insert('PartsOfSpeech', {'part': partOfSpeech});
+      return newId;
+    }
+  }
+
+  Future<Map<String, List<String>>> getWordDefsMapping(String word) async {
+    Database db = await database;
+    List<Map<String, dynamic>> entries = await db.rawQuery('''
+    SELECT PartsOfSpeech.part, Definitions.definition
+    FROM Definitions
+    JOIN Words ON Definitions.word_id = Words.id
+    JOIN PartsOfSpeech ON Definitions.part_of_speech_id = PartsOfSpeech.id
+    WHERE Words.word = ?
+    ''', [word]);
+
+    // mapping of
+    Map<String, List<String>> pdDict = {};
+
+    for (var entry in entries) {
+      String partOfSpeech = entry['part_of_speech'];
+      String definition = entry['definition'];
+
+      if (pdDict.containsKey(partOfSpeech)) {
+        pdDict[partOfSpeech]!.add(definition);
+      } else {
+        pdDict[partOfSpeech] = [definition];
+      }
+    }
+    print(pdDict);
+    return pdDict;
   }
 }
