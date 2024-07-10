369546d74c17190ced1b9257e80f7b17fe34a050
tankk <tankk@student.unimelb.edu.au>
2024-07-10 23:01:33 +1000

    updated sdk version for andriod compat

--- toki_pona_learner/lib/src/database/db_helper.dart
+++ toki_pona_learner/lib/src/database/db_helper.dart
@@ -1,4 +1,6 @@
 // import 'package:sqflite/sqflite.dart';
+import 'dart:math';
+
 import 'package:flutter/services.dart';
 
 import './database_sql.dart';
@@ -6,12 +8,16 @@ import 'dart:io';
 import 'dart:async';
 import 'package:path_provider/path_provider.dart';
 import 'package:sqflite_common_ffi/sqflite_ffi.dart';
+
+import 'package:flutter/foundation.dart' show kIsWeb, defaultTargetPlatform;
+import 'package:path/path.dart';
 import 'package:csv/csv.dart';
 
 //import models
 import '../models/word.dart';
 import '../models/part_of_speech.dart';
 import '../models/definition.dart';
+import '../models/practice_type.dart';
 import '../models/word_fact.dart';
 
 class DatabaseHelper {
@@ -20,9 +26,11 @@ class DatabaseHelper {
   static Database? _database;
 
   DatabaseHelper._createInstance() {
-    // init sqflite for desktop
-    sqfliteFfiInit();
-    databaseFactory = databaseFactoryFfi;
+    if (!kIsWeb &&
+        (Platform.isWindows || Platform.isLinux || Platform.isMacOS)) {
+      sqfliteFfiInit();
+      databaseFactory = databaseFactoryFfi;
+    }
   }
 
   factory DatabaseHelper() {
@@ -160,6 +168,56 @@ class DatabaseHelper {
     return defsDictList;
   }
 
+  // get a randomWordFact in the DB
+  Future<WordFact> getRandomWordFact(QuestionFormat questionFormat) async {
+    Database db = await database;
+
+    List<Map<String, dynamic>> numWordQuery =
+        await db.rawQuery("SELECT max(id) FROM Words");
+    int numWords = numWordQuery.first['max(id)'];
+
+    int randomID = Random().nextInt(numWords) + 1;
+    print(randomID);
+    String dontShow = (questionFormat == QuestionFormat.symbols)
+        ? "dont_show_sym"
+        : "dont_show_def";
+
+    List<Map<String, dynamic>> entries = await db.rawQuery('''
+    SELECT Words.id AS word_id, Words.word, PartsOfSpeech.part, Definitions.definition
+      FROM Words
+      LEFT JOIN Definitions ON Definitions.word_id = Words.id
+      LEFT JOIN PartsOfSpeech ON Definitions.part_of_speech_id = PartsOfSpeech.id
+      WHERE word_id = $randomID AND Words.$dontShow = 0
+    ''');
+
+    // mapping
+    Map<String, List<String>> definitionsMap = {};
+    WordFact wordFact;
+
+    int? id = entries.first['word_id'];
+    String word = entries.first['word'];
+    Word finalWord = Word(id: id, word: word);
+
+    // for each entry in the database
+    for (var entry in entries) {
+      String partOfSpeech = entry['part'];
+      String definition = entry['definition'];
+
+      // is this part of speech already in the mapping?
+      if (definitionsMap.containsKey(partOfSpeech)) {
+        // yes, add to map
+        definitionsMap[partOfSpeech]!.add(definition);
+      } else {
+        // no, make it the first entry
+        definitionsMap[partOfSpeech] = [definition];
+      }
+    }
+
+    wordFact = WordFact(word: finalWord, defsDict: definitionsMap);
+
+    return wordFact;
+  }
+
   // get a WordFact from one word
   Future<WordFact> getWordFact(Word word) async {
     Database db = await database;
@@ -211,5 +269,153 @@ class DatabaseHelper {
       words.add(Word.fromMap(map));
     }
     return words;
+  }
+
+  // get a list of wordFacts for pratice quiz
+  Future<List<WordFact>> getWordFacts(
+      QuestionFormat questionFormat, int limit) async {
+    List<WordFact> wordFacts = [];
+    wordFacts = await getWordFactEntries(questionFormat, limit);
+    return wordFacts;
+  }
+
+  Future<List<WordFact>> getWordFactEntries(
+      QuestionFormat questionFormat, int limit) async {
+    final Database db = await database;
+
+    String dontShow = (questionFormat == QuestionFormat.symbols)
+        ? "dont_show_sym"
+        : "dont_show_def";
+
+    List<Map<String, dynamic>> entries = await db.rawQuery('''
+      SELECT Words.id AS word_id, Words.word, PartsOfSpeech.part, Definitions.definition
+      FROM Words
+      LEFT JOIN Definitions ON Definitions.word_id = Words.id
+      LEFT JOIN PartsOfSpeech ON Definitions.part_of_speech_id = PartsOfSpeech.id
+      WHERE Words.$dontShow = 0
+      ORDER BY RANDOM()
+      LIMIT $limit
+      
+    ''');
+
+    // mapping
+    Map<int, Word> wordMap = {};
+    Map<int, Map<String, List<String>>> definitionsMap = {};
+
+    // for each entry in the database
+    for (var entry in entries) {
+      int wordId = entry['word_id'];
+      String wordText = entry['word'];
+      String partOfSpeech = entry['part'];
+      String definition = entry['definition'];
+
+      // are we processing a new word?
+      if (!wordMap.containsKey(wordId)) {
+        // yes a new word
+        wordMap[wordId] = Word(id: wordId, word: wordText);
+        definitionsMap[wordId] = {};
+      }
+      // is this part of speech already in the mapping?
+      if (definitionsMap[wordId]!.containsKey(partOfSpeech)) {
+        // yes, add to map
+        definitionsMap[wordId]![partOfSpeech]!.add(definition);
+      } else {
+        // no, make it the first entry
+        definitionsMap[wordId]![partOfSpeech] = [definition];
+      }
+    }
+    List<WordFact> wordFacts = [];
+    wordFacts = wordMap.entries.map((entry) {
+      int wordId = entry.key;
+      Word word = entry.value;
+      Map<String, List<String>> definitions = definitionsMap[wordId]!;
+      return WordFact(word: word, defsDict: definitions);
+    }).toList();
+    return wordFacts;
+  }
+
+  Future<List<int>> getRandomWordIDList(QuestionFormat questionFormat) async {
+    Database db = await database;
+    String dontShow = (questionFormat == QuestionFormat.symbols)
+        ? "dont_show_sym"
+        : "dont_show_def";
+
+    List<Map<String, dynamic>> numWordQuery = await db.rawQuery('''
+      SELECT max(id) FROM Words
+      WHERE Words.$dontShow = 0
+    ''');
+    int numWords = numWordQuery.first['max(id)'];
+    List<int> questionList = List<int>.generate(numWords, (i) => i + 1);
+    questionList.shuffle(Random());
+
+    return questionList;
+  }
+
+  // get a wordFact in the DB by wordID
+  Future<WordFact> getWordFactByID(int wordID) async {
+    Database db = await database;
+
+    List<Map<String, dynamic>> entries = await db.rawQuery('''
+    SELECT Words.id AS word_id, Words.word, PartsOfSpeech.part, Definitions.definition
+      FROM Words
+      LEFT JOIN Definitions ON Definitions.word_id = Words.id
+      LEFT JOIN PartsOfSpeech ON Definitions.part_of_speech_id = PartsOfSpeech.id
+      WHERE word_id = $wordID
+      
+    ''');
+
+    // mapping
+    Map<String, List<String>> definitionsMap = {};
+    WordFact wordFact;
+
+    int? id = entries.first['word_id'];
+    String word = entries.first['word'];
+    Word finalWord = Word(id: id, word: word);
+
+    // for each entry in the database
+    for (var entry in entries) {
+      String partOfSpeech = entry['part'];
+      String definition = entry['definition'];
+
+      // is this part of speech already in the mapping?
+      if (definitionsMap.containsKey(partOfSpeech)) {
+        // yes, add to map
+        definitionsMap[partOfSpeech]!.add(definition);
+      } else {
+        // no, make it the first entry
+        definitionsMap[partOfSpeech] = [definition];
+      }
+    }
+
+    wordFact = WordFact(word: finalWord, defsDict: definitionsMap);
+
+    return wordFact;
+  }
+
+  Future<List<WordFact>> getQuizOptions(WordFact original) async {
+    List<WordFact> options = [];
+
+    Database db = await database;
+
+    List<Map<String, dynamic>> randomWords = await db.rawQuery('''
+      SELECT Words.id as word_id
+      FROM Words
+      WHERE word_id != ${original.word.id}
+      ORDER BY RANDOM()
+      LIMIT 3
+    ''');
+
+    List<WordFact> quizWordFacts = [];
+    for (var randomWord in randomWords) {
+      int wordID = randomWord['word_id'];
+      WordFact option = await getWordFactByID(wordID);
+      quizWordFacts.add(option);
+    }
+
+    options = quizWordFacts;
+    options.add(original);
+    options.shuffle(Random());
+
+    return options;
   }
 }
