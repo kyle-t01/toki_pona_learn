b70b0c8591b4ada9d01354f1e606029be9a38022
tankk <tankk@student.unimelb.edu.au>
2024-07-11 12:57:24 +1000

    app theme

--- toki_pona_learner/lib/main.dart
+++ toki_pona_learner/lib/main.dart
@@ -20,9 +20,39 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Toki Pona Learning App',
       theme: ThemeData(
-        colorScheme:
-            ColorScheme.fromSeed(seedColor: Color.fromARGB(255, 7, 83, 90)),
         useMaterial3: false,
+        brightness: Brightness.dark,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 7, 83, 90),
+          brightness: Brightness.dark,
+        ),
+        scaffoldBackgroundColor: Colors.black,
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color.fromARGB(255, 10, 56, 61),
+          titleTextStyle: TextStyle(
+            color: Colors.white,
+            fontSize: 20,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        textTheme: const TextTheme(
+          bodyLarge: TextStyle(color: Colors.white),
+          bodyMedium: TextStyle(color: Colors.white),
+          bodySmall: TextStyle(color: Colors.white),
+        ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+          style: ElevatedButton.styleFrom(
+            backgroundColor: const Color.fromARGB(255, 7, 83, 90),
+            foregroundColor: Colors.white,
+            textStyle: const TextStyle(fontSize: 18),
+          ),
+        ),
+        cardTheme: const CardTheme(
+          color: Color.fromARGB(255, 18, 18, 18), // Dark background for cards
+          shadowColor: Colors.black,
+          elevation: 5,
+          margin: EdgeInsets.all(8),
+        ),
       ),
       home: const MainMenu(),
     );
